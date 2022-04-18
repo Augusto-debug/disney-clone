1710baef19bca273e22ee3df51b89b7149219531
Augusto-debug <augustocesarrezende64@gmail.com>
2022-04-18 14:38:22 -0300

    ADD: ImgSlider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
+import Viewers from './Viewers';
 
 function Home() {
   return (
     <Container>
-        <h1>Hwllo</h1>
+        <ImgSlider />
+        <Viewers />
     </Container>
   )
 }
@@ -12,5 +15,23 @@ function Home() {
 export default Home;
 
 const Container = styled.main`
+   min-height: calc(100vh - 70px);
+   padding: 10px calc(3.5vw + 5px);
+   position: relative;
+   overflow-x : hidden;
    
+   &:before {
+    content: "";
+    background-image: url('/images/home-background.png');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+   }
 `;
